6541a706e88e9cc3f55e707a6ed238be8ef0c9a9
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-09 02:58:43 +0530

    Card Component

---
+++ src/components/Card.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { IoMdDownload } from 'react-icons/io';
+import { IoCloseSharp } from 'react-icons/io5';
+import { motion } from "framer-motion"
+
+
+
+
+const Card = ({ data, reference }) => {
+  return (
+    <div>
+        <motion.div drag dragConstraints={reference} className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden' >
+        <FaRegFileAlt />
+
+        <p className='text-sm leading-tight mt-5 font-semibold'>{data.description}</p>
+
+        <div className='footer absolute bottom-0 w-full  left-0'>
+            <div className='flex items-center justify-between py-3 px-8 mb-3'>
+                <h5>{data.filesize}</h5>
+                <span className='w-7 h-7 bg-zinc-700 rounded-full flex items-center justify-center'>
+                    {
+                        data.close ? <IoCloseSharp size=".8em" /> : <IoMdDownload size=".8em" />
+
+                    }
+                    
+                </span>
+            </div>
+            {
+                data.tag.isOpen && ( <div className= {`tag w-full py-4 ${data.tag.tagColor ==="blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+                                        <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+                                    </div>)
+                                
+            }
+            
+        </div>
+
+
+        footer 
+            data 
+            tag
+        </motion.div>
+
+    </div>
+  )
+}
+
+export default Card
